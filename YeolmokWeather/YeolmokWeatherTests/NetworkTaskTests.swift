daf2db8ead22cbfadec5870f989e9579570a6ed6
Donguk Kim <ehddnr73@naver.com>
2022-12-17 11:23:45 +0900

    코드 정리

--- YeolmokWeather/YeolmokWeatherTests/NetworkTaskTests.swift
+++ YeolmokWeather/YeolmokWeatherTests/NetworkTaskTests.swift
@@ -20,22 +20,6 @@ final class NetworkTaskTests: XCTestCase {
         self.networkManager = networkManager
     }
     
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-    
     func testGetReverseGeocodingURL() {
         let location = CLLocationCoordinate2D(latitude: CLLocationDegrees(111.111), longitude: CLLocationDegrees(222.222))
         let url = networkManager.getReverseGeocodingURL(with: location)
@@ -110,12 +94,4 @@ final class NetworkTaskTests: XCTestCase {
             }
         }
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
